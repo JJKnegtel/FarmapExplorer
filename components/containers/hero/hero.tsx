d03144d5e8472541f9c38c6b7fdb4af3f0730ea6
Spoonman <155550137+JJKnegtel@users.noreply.github.com>
2024-06-25 14:12:03 +0200

    Update hero.tsx

--- components/containers/hero/hero.tsx
+++ components/containers/hero/hero.tsx
@@ -4,14 +4,14 @@ export const Hero = () => {
   return (
     <section className="mx-auto flex max-w-4xl flex-col items-center gap-2 py-8 md:py-12 md:pb-8 lg:py-12 lg:pb-12 container">
       <Badge className="my-2" variant="secondary">
-        Currently focused on Solana with some data on Degen, Farcaster & Base.
+        Discover and explore the latest projects in Base, farcaster, and degen ecosystems. 
       </Badge>
 
       <h1 className="scroll-m-20 text-balance text-center text-4xl font-extrabold leading-tight tracking-tight lg:text-5xl lg:leading-[1.1]">
-        Powering Discoverability of Assets, Products, and Services in Web3.
+        Explore the Future of the Farcaster Ecosystem with FarMap
       </h1>
       <p className="max-w-xl text-center text-lg font-light text-foreground">
-        We provide validated & structured Metadata to Data Platforms & LLMs.
+        We provide the most complete & current information and dataset of projects within the Base, Farcaster, and Degen ecosystem.
       </p>
     </section>
   );
