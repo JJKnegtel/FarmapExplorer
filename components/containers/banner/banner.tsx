cd81aa05e14c1628e4e7d61327cd34b561728b24
Spoonman <155550137+JJKnegtel@users.noreply.github.com>
2024-06-26 13:22:30 +0200

    Update banner.tsx

--- components/containers/banner/banner.tsx
+++ components/containers/banner/banner.tsx
@@ -2,7 +2,7 @@ export const Banner = () => {
   return (
     <section className="flex justify-center bg-foreground p-2">
       <span className="text-center text-sm font-light text-muted/70">
-        NEW: Curated directory of Farcaster, Base & Degen Projects
+        Discover and explore the latest projects in Farcaster, Base and Degen ecosystems.
       </span>
     </section>
   );
